3c4905f5b2ec9e4e813262dd72102cf83fdc931f
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-09-02 12:37:21 -0600

    api routes

--- db/store.js
+++ db/store.js
@@ -1,3 +1,24 @@
 const fs = require("fs");
 const util = require("util");
 const { v4 } = require("uuid");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+  write(notes) {
+    return writeFileAsync("db/db.json", JSON.stringify(notes));
+  }
+  getNotes() {
+    return this.read()
+    .then((notes) => {
+      return JSON.parse(notes);
+    }) 
+  }
+};
+
+
+module.exports = new Store();
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
+const store = require("../db/store.js");
+
+router.get("/notes", (req, res) => {
+  store.getNotes()
+    .then((results) => {
+      return res.json(results)
+    })
+    .catch((err) =>{
+      res.status(500).json(err)
+    })
+
+})
 
 module.exports = router;
